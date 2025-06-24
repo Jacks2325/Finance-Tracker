a8ce959ce3920f3f4c3e11ab420874bddcc695d4
Shivam Jawarkar <shivamjawarkar2325@gmail.com>
2025-06-24 12:02:38 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+
+const CACHE_NAME = "finance-dashboard-v1";
+const urlsToCache = [
+  "/",
+  "/PWA_Enhanced_Tracker.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
